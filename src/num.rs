71d0be58b6c2dfec405f2baa00bbafa0fa008a2b
Joel Wejdenstål <joel.wejdenstal@gmail.com>
2025-10-08 16:24:37 +0200

    impl simple bitops

--- src/num.rs
+++ src/num.rs
@@ -1,10 +1,5 @@
 use crate::value::Value;
 
-pub trait ArithOp {
-    fn int(lhs: i64, rhs: i64) -> Value;
-    fn float(lhs: f64, rhs: f64) -> Value;
-}
-
 #[inline(always)]
 pub fn op_arith<Op: ArithOp>(lhs: Value, rhs: Value) -> Option<Value> {
     if let (Value::Integer(lhs), Value::Integer(rhs)) = (lhs, rhs) {
@@ -24,6 +19,11 @@ pub fn op_arith<Op: ArithOp>(lhs: Value, rhs: Value) -> Option<Value> {
     };
 
     Some(Op::float(lhs, rhs))
+}
+
+pub trait ArithOp {
+    fn int(lhs: i64, rhs: i64) -> Value;
+    fn float(lhs: f64, rhs: f64) -> Value;
 }
 
 pub struct Add;
@@ -121,5 +121,71 @@ impl ArithOp for IDiv {
     #[inline(always)]
     fn float(lhs: f64, rhs: f64) -> Value {
         Value::Integer((lhs / rhs).floor() as i64)
+    }
+}
+
+#[inline(always)]
+pub fn op_bit<Op: BitOp>(lhs: Value, rhs: Value) -> Option<Value> {
+    let lhs = match lhs {
+        Value::Integer(v) => v,
+        Value::Float(v) if v.trunc() == v => v as i64,
+        _ => return None,
+    };
+
+    let rhs = match rhs {
+        Value::Integer(v) => v,
+        Value::Float(v) if v.trunc() == v => v as i64,
+        _ => return None,
+    };
+
+    Some(Op::int(lhs, rhs))
+}
+
+pub trait BitOp {
+    fn int(lhs: i64, rhs: i64) -> Value;
+}
+
+pub struct BAnd;
+
+impl BitOp for BAnd {
+    #[inline(always)]
+    fn int(lhs: i64, rhs: i64) -> Value {
+        Value::Integer(lhs & rhs)
+    }
+}
+
+pub struct BOr;
+
+impl BitOp for BOr {
+    #[inline(always)]
+    fn int(lhs: i64, rhs: i64) -> Value {
+        Value::Integer(lhs | rhs)
+    }
+}
+
+pub struct BXor;
+
+impl BitOp for BXor {
+    #[inline(always)]
+    fn int(lhs: i64, rhs: i64) -> Value {
+        Value::Integer(lhs ^ rhs)
+    }
+}
+
+pub struct Shl;
+
+impl BitOp for Shl {
+    #[inline(always)]
+    fn int(lhs: i64, rhs: i64) -> Value {
+        Value::Integer(lhs.wrapping_shl(rhs as u32))
+    }
+}
+
+pub struct Shr;
+
+impl BitOp for Shr {
+    #[inline(always)]
+    fn int(lhs: i64, rhs: i64) -> Value {
+        Value::Integer(lhs.wrapping_shr(rhs as u32))
     }
 }
