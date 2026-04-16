78257488a95a23a9ec302f927b385b8ffd4f3563
Joel Wejdenstål <joel.wejdenstal@gmail.com>
2026-04-16 16:17:24 +0200

    add nbody parser test

--- src/compiler/snapshot_tests.rs
+++ src/compiler/snapshot_tests.rs
@@ -224,7 +224,6 @@ test!(function, "test-files/function.lua");
 test!(hello, "test-files/hello.lua");
 test!(if, "test-files/if.lua");
 test!(literal, "test-files/literal.lua");
-//test!(metalua, "test-files/metalua.lua");
 test!(nbody, "test-files/nbody.lua");
 test!(op_prec, "test-files/op_prec.lua");
 test!(primes, "test-files/primes.lua");
